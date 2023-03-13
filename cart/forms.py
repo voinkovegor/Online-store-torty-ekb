660527e94bb97dafb92fa0210262aff7b9435dcf
voinkovegor <voinkov-egor@mail.ru>
2023-03-13 16:44:00 +0500

    Updated design, added new pages

--- cart/forms.py
+++ cart/forms.py
@@ -7,10 +7,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                                 choices=PRODUCT_QUANTITY_CHOICES,
-                                coerce=int)
+                                coerce=int,
+                                label='Вес, кг')
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
-
-    class Meta:
-        verbose_name = 'Количество'
--- orders/tasks.py
+++ orders/tasks.py
@@ -13,7 +13,8 @@ def order_created(order_id):
     subject = f'Заказ № {order.id}'
     message = f'Уважаемый(-ая) {order.first_name},\n\n' \
               f'Вы успешно оформили заказ № {order.id}. ' \
-              f'В ближайшее время с вами свяжется менеджер.'
+              f'В ближайшее время с вами свяжется менеджер.\n' \
+              f'Телефон для связи +7(999)999-99-99'
     mail_sent = send_mail(subject,
                           message,
                           None,  # от кого письмо
