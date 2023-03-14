3638220575cbae5ad3b2ebf6e66f484ca2b7c06f
voinkovegor <voinkov-egor@mail.ru>
2023-03-14 19:09:20 +0500

    Replace rabbitMQ on redis, add docker files

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,5 +1,4 @@
 from celery import shared_task
-from django.core.mail import send_mail
 from .models import Order
 
 from io import BytesIO
--- torty/celery.py
+++ torty/celery.py
@@ -1,9 +1,11 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'torty.settings')
 
 app = Celery('torty')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
