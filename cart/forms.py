e060ccfa6463f04e2c13c38532e482bdfddaa45e
voinkovegor <voinkov-egor@mail.ru>
2023-03-16 11:38:47 +0500

    Merged with quantity branch

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 
-#PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 11)]
 l = [0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]
 PRODUCT_QUANTITY_CHOICES = list(enumerate(map(str, l), start=1))
 
